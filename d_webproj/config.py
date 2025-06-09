73802a0dbc6c21abd01a9993efd3148fcef1a472
drexxm <65310167@go.buu.ac.th>
2025-06-09 11:18:16 +0700

    problem with MSSQL

--- d_webproj/config.py
+++ d_webproj/config.py
@@ -1,14 +1,14 @@
 import os
 
+# old DB SQLite
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "this_should_be_a_secret_key"
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'instance', 'database.db')}"
+    # SECRET_KEY = os.environ.get("SECRET_KEY") or "this_should_be_a_secret_key"
+    # SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'instance', 'database.db')}"
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# MSSQL
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://<USERNAME>:<PASSWORD>@<SERVER>/<DATABASE>?driver=ODBC+Driver+17+for+SQL+Server"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# import os
-
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'database.db')}"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "your_secret_key"
