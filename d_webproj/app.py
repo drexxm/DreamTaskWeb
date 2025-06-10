630aa023001bb3c7ecfc6094071db1c2592037c8
drexxm <65310167@go.buu.ac.th>
2025-06-10 11:12:23 +0700

    connecting MySQL

--- d_webproj/app.py
+++ d_webproj/app.py
@@ -27,7 +27,10 @@ app.register_blueprint(auth)
 app.register_blueprint(task_bp)
 
 # ✅ สร้างตาราง
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
--- d_webproj/config.py
+++ d_webproj/config.py
@@ -2,13 +2,10 @@ import os
 
 # old DB SQLite
 class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://3306:SmartAccess@2025@localhost/taskmanager1"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "your_secret_key"
+
     # SECRET_KEY = os.environ.get("SECRET_KEY") or "this_should_be_a_secret_key"
     # SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'instance', 'database.db')}"
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# MSSQL
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://<USERNAME>:<PASSWORD>@<SERVER>/<DATABASE>?driver=ODBC+Driver+17+for+SQL+Server"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key"
